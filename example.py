30b7a8534ffef561e63d49ad9ee7ee7010e50385
ErikBoesen <me@erikboesen.com>
2019-06-28 22:11:16 -0400

    Demonstrate new syntax in example

--- example.py
+++ example.py
@@ -7,12 +7,12 @@ api = yalelaundry.YaleLaundry(os.environ['YALE_API_KEY'])
 # You can get anything directly by ID/key
 rooms = api.rooms()
 availability = api.availability(rooms[1].id)
-total = api.total(rooms[1].id)
+total = api.totals(rooms[1].id)
 appliances = api.appliances(rooms[1].id)
 status = api.status(appliances[0].key)
 
 # Or you can use a more intuitive syntax
-room = rooms[0]
+room = rooms[8]
 availability = room.availability
 totals = room.totals
-print("There are %d/%d dryers available at %s." % (availability.dryers, totals.dryers, room.name))
+print("There are %d/%d dryers available at %s." % (availability.dryer, totals.dryer, room.name))

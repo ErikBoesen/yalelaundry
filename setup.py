a86b3f2575f170806f6f6bce17f3afca6c4e8d77
ErikBoesen <me@erikboesen.com>
2019-07-20 12:01:08 -0400

    Bump to 0.3.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='yalelaundry',
-      version='0.2.1',
+      version='0.3.0',
       description='Library for fetching data from the Yale Laundry APIs.',
       url='https://github.com/ErikBoesen/yalelaundry',
       author='Erik Boesen',
